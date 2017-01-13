c62ec9ae0d4a4ea018c7e6505b359ba69d4f734d
jcbm <jcb.madsen@gmail.com>
2017-01-13 17:04:38 +0100

    added printState method to WorkerInfo

--- Evaluation.java
+++ Evaluation.java
@@ -3,14 +3,37 @@
  */
 public class Evaluation {
     private final String subtask;
-    private final int score;
+    private final EvaluationScore score;
 
     public Evaluation(String subtaskId, int time, int averageTime) {
         this.subtask = subtaskId;
-        this.score = calculateScore(time, averageTime);
+        this.score = calculateScore(time, averageTime); // fixme - it's possible, but horrible practice to call same-object methods in constructor
     }
 
-    private int calculateScore(int time, int averageTime) {
-return 0; // todo: time > averageTime;
+    private EvaluationScore calculateScore(int workersTime, int averageWorkerTime) {
+ // fixme: silly measure
+        EvaluationScore score = null;
+        if (workersTime == averageWorkerTime) {
+            score = EvaluationScore.AVERAGE;
+        }
+        if (workersTime > averageWorkerTime){
+            score = EvaluationScore.GOOD;
+        }
+        if (workersTime < averageWorkerTime) {
+            score = EvaluationScore.BAD;
+        }
+        return score;
     }
+
+    public String getSubtask() {
+        return subtask;
+    }
+
+    public EvaluationScore getScore() {
+        return score;
+    }
+}
+
+enum EvaluationScore {
+    BAD, AVERAGE, GOOD
 }
--- WorkerInfo.java
+++ WorkerInfo.java
@@ -12,7 +12,7 @@ public class WorkerInfo {
     private int port;
     // ParentID to HashMap<subtask id to subtaskData> - tells which tasks are waiting to processed/are currently processed
     private HashMap<String, HashMap<String, SubTaskData>> activeTasks = new HashMap<>(); //
-    // containedTasks tells which tasks have been completed at the worker, but not yet at the reducer
+    // containedTasks tells which tasks have been completed at the worker, but not yet at the reducer. We don't need a hashMap here, as we send these in a all or none fashion.
     private HashMap<String, ArrayList<SubTaskData>> containedTasks = new HashMap<>(); //
     private HashMap<String, HashMap<String, SubTaskData>> historicalTasks = new HashMap<>(); //
     private ArrayList<Evaluation> evaluations = new ArrayList();
@@ -42,7 +42,6 @@ public class WorkerInfo {
         tasksForParent.put(subtaskID, subtask);
     }
 
-    // When we recieve a task finished message - when a final result is recieved -
     // we inactivate a task when it is finished, that is, it is removed from the activeset. However, to know
     public void inactivateTask(String parentID, String subtaskID, int completionTime) { // this is obtained in the scheduler where we have SubtaskID:subtask (not data) map --> getParent
         //should never return null, so we can be sure we get a collection
@@ -54,16 +53,18 @@ public class WorkerInfo {
         HashMap<String, SubTaskData> compledTasksForParent = historicalTasks.get(parentID);
         if (compledTasksForParent == null) {
             compledTasksForParent = new HashMap<>();
+            historicalTasks.put(parentID, compledTasksForParent);
+
         }
         compledTasksForParent.put(subtaskID, subTaskData);
-        historicalTasks.put(parentID, compledTasksForParent);
         subTaskDataHashMap.remove(subtaskID);
         ArrayList<SubTaskData> notFinalizedTasks = containedTasks.get(parentID);
         if (notFinalizedTasks == null) {
             notFinalizedTasks = new ArrayList<>();
+            containedTasks.put(parentID, notFinalizedTasks);
         }
         notFinalizedTasks.add(subTaskData);
-        containedTasks.put(parentID, notFinalizedTasks);
+        //containedTasks.put(parentID, notFinalizedTasks);
         // maybe  activeTasks.put(parentID, subTaskDataHashMap);
     }
 
@@ -103,4 +104,68 @@ public class WorkerInfo {
         containedTasks.remove(parentTask);
 
     }
+
+    /*
+    For debugging - Get all stored values in a user-friendly format
+     */
+    public void printState() {
+        System.out.println("---STATE OF WORKER " + guid + " ---");
+        String active = isInactive() ? "not active" : "active";
+        System.out.println("Worker is" + active);
+        System.out.println("Worker is yet to complete:");
+        // All tasks that the reducer is either working on currently or have queued for processing
+        Iterator it = activeTasks.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            String parentTask = (String) pair.getKey();
+            System.out.println("Subtasks exists for parent " + parentTask + ":");
+            HashMap<String, SubTaskData> subtasksForParent = (HashMap) pair.getValue();
+            Iterator subtaskIterator = subtasksForParent.entrySet().iterator();
+            while (subtaskIterator.hasNext()) {
+                Map.Entry subTaskIdSubtaskPair = (Map.Entry) subtaskIterator.next();
+                String subTask = (String) subTaskIdSubtaskPair.getKey();
+                System.out.println("-" + subTask);
+            }
+            System.out.println(System.lineSeparator());
+        }
+        System.out.println("Worker is storing results for");
+        // All tasks that have been completed but not yet processed by the assigned Reducer
+        it = containedTasks.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            String parentTask = (String) pair.getKey();
+            System.out.println("Results exist for parent " + parentTask);
+            ArrayList<SubTaskData> subtasksForParent = (ArrayList) pair.getValue();
+            for (SubTaskData subTask : subtasksForParent) {
+                System.out.println("-" + subTask.getId());
+            }
+            System.out.println(System.lineSeparator());
+        }
+        System.out.println("Worker has contributed to these completed tasks");
+
+        // All tasks that have been processed by the assigned Reducer
+        it = historicalTasks.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            String parentTask = (String) pair.getKey();
+            System.out.println("Subtasks exists for parent " + parentTask + ":");
+            HashMap<String, SubTaskData> subtasksForParent = (HashMap) pair.getValue();
+            Iterator subtaskIterator = subtasksForParent.entrySet().iterator();
+            while (subtaskIterator.hasNext()) {
+                Map.Entry subTaskIdSubtaskPair = (Map.Entry) subtaskIterator.next();
+                String subTask = (String) subTaskIdSubtaskPair.getKey();
+                System.out.println("-" + subTask);
+            }
+            System.out.println(System.lineSeparator());
+        }
+        System.out.println("The worker's evaluation scores are'");
+        System.out.println("-----Task-----  ||  ---Score---  ");
+        for (Evaluation evaluation : evaluations) {
+            String subtaskId = evaluation.getSubtask();
+            EvaluationScore evaluationScore = evaluation.getScore();
+            System.out.println(subtaskId + " | " + evaluationScore);
+        }
+    }
+
+
 }
